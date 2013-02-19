2608948db1bbe7afd09df59d16fb838b86ace3f1
Simon Timms <simon.timms@worleyparsons.com>
2013-02-18 21:50:46 -0700

    Scaling and transaltion all wrong

---
+++ map.js
@@ -0,0 +1,21 @@
+var OpenData;
+(function (OpenData) {
+    var CalgaryMap = (function () {
+        function CalgaryMap() { }
+        CalgaryMap.prototype.render = function () {
+            var xy = d3.geo.albers(0.25).translate([
+                -1000, 
+                1800
+            ]);
+            var path = d3.geo.path().projection(xy);
+            var vis = d3.select("#map").append("svg:svg").attr("width", 900).attr("height", 900);
+            d3.json("data.brighton.json", function (json) {
+                vis.append("svg:g").attr("class", "tracts").selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr("fill-opacity", 0.5).attr("fill", function (d) {
+                    return "black";
+                }).attr("stroke", "#222");
+            });
+        };
+        return CalgaryMap;
+    })();
+    OpenData.CalgaryMap = CalgaryMap;    
+})(OpenData || (OpenData = {}));
---
+++ map.ts
@@ -0,0 +1,30 @@
+declare var d3: any;
+
+module OpenData
+{
+  export class CalgaryMap{
+
+    render(){
+        var xy = d3.geo.albers(.25).translate([-1000,1800]);//.scale(5000);//.translate([-900,950]);//.scale(28000);
+        var path = d3.geo.path().projection(xy);
+
+        var vis = d3.select("#map")
+          .append("svg:svg")
+          .attr("width", 900)
+          .attr("height", 900);
+
+        d3.json( "data.brighton.json", (json) =>
+        {
+          vis.append("svg:g")
+            .attr("class", "tracts")
+          .selectAll("path")
+            .data(json.features)
+          .enter().append("svg:path")
+            .attr("d", path)
+            .attr("fill-opacity", 0.5)
+            .attr("fill", (d) => {return "black";})
+            .attr("stroke", "#222")
+        })
+    }
+  }
+}
